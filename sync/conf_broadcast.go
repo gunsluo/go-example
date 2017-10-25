6adcf29a18a9e8ad47cac292352bc979d038ddab
jerrylou <gunsluo@gmail.com>
2017-10-25 10:31:10 +0800

    fix broadcast
    Signed-off-by: jerrylou <gunsluo@gmail.com>

--- sync/conf_broadcast.go
+++ sync/conf_broadcast.go
@@ -7,21 +7,30 @@ import (
 )
 
 func work(cond *sync.Cond, i int) {
-	fmt.Println("work", i)
 	cond.L.Lock()
 	cond.Wait()
 	cond.L.Unlock()
+	fmt.Println("work", i)
 }
 
 func main() {
+	var wg sync.WaitGroup
 	var cond = sync.NewCond(new(sync.Mutex))
 	for i := 0; i < 10; i++ {
-		go work(cond, i)
+		wg.Add(1)
+		go func(i int) {
+			cond.L.Lock()
+			cond.Wait()
+			cond.L.Unlock()
+			fmt.Println("work", i)
+			wg.Done()
+		}(i)
 	}
 	// 下面的 sleep 很重要
-	time.Sleep(time.Duration(2) * time.Second)
+	time.Sleep(time.Second)
 	fmt.Println("Wake up")
 	cond.Broadcast()
-	//time.Sleep(time.Duration(20) * time.Second)
+	//cond.Signal()
+	wg.Wait()
 	fmt.Println("Done")
 }
