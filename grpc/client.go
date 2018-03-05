b35afb24bd811d0055e71e3403348dd26238741c
luoji <gunsluo@gmail.com>
2018-03-05 11:12:45 +0800

    grpc interceptor study
    Signed-off-by: luoji <gunsluo@gmail.com>

--- grpc/client.go
+++ grpc/client.go
@@ -17,7 +17,10 @@ const (
 
 func main() {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	//conn, err := grpc.Dial(address, grpc.WithInsecure())
+	conn, err := grpc.Dial(address, grpc.WithInsecure(),
+		grpc.WithUnaryInterceptor(UnaryClientInterceptor),
+		grpc.WithStreamInterceptor(StreamClientInterceptor))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -36,4 +39,18 @@ func main() {
 		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Greeting: %s", r.Message)
+}
+
+func UnaryClientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	log.Printf("before invoker. method: %+v, request:%+v", method, req)
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	log.Printf("after invoker. reply: %+v", reply)
+	return err
+}
+
+func StreamClientInterceptor(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	log.Printf("before invoker. method: %+v, StreamDesc:%+v", method, desc)
+	clientStream, err := streamer(ctx, desc, cc, method, opts...)
+	log.Printf("before invoker. method: %+v", method)
+	return clientStream, err
 }
--- grpc/server.go
+++ grpc/server.go
@@ -28,11 +28,28 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	//s := grpc.NewServer()
+	s := grpc.NewServer(grpc.StreamInterceptor(StreamServerInterceptor),
+		grpc.UnaryInterceptor(UnaryServerInterceptor))
 	pb.RegisterGreeterServer(s, &server{})
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func UnaryServerInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Printf("before handling. Info: %+v", info)
+	resp, err := handler(ctx, req)
+	log.Printf("after handling. resp: %+v", resp)
+	return resp, err
+}
+
+// StreamServerInterceptor is a gRPC server-side interceptor that provides Prometheus monitoring for Streaming RPCs.
+func StreamServerInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	log.Printf("before handling. Info: %+v", info)
+	err := handler(srv, ss)
+	log.Printf("after handling. err: %v", err)
+	return err
 }
