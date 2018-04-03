a8965ec0e69669b1b127dcaa17e749840c2a75bd
luoji <gunsluo@gmail.com>
2018-04-03 19:21:42 +0800

    grpc gateway
    Signed-off-by: luoji <gunsluo@gmail.com>

---
+++ grpc/gateway/cmd/server.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/gunsluo/go-example/grpc/gateway/server"
+	"github.com/spf13/cobra"
+)
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Run the gRPC hello-world server",
+	Run: func(cmd *cobra.Command, args []string) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println("Recover error : %v", err)
+			}
+		}()
+
+		server.Serve()
+	},
+}
+
+func init() {
+	serverCmd.Flags().StringVarP(&server.ServerPort, "port", "p", "50052", "server port")
+	serverCmd.Flags().StringVarP(&server.CertPemPath, "cert-pem", "", "./certs/server.pem", "cert pem path")
+	serverCmd.Flags().StringVarP(&server.CertKeyPath, "cert-key", "", "./certs/server.key", "cert key path")
+	serverCmd.Flags().StringVarP(&server.CertName, "cert-name", "", "grpc server name", "server's hostname")
+	rootCmd.AddCommand(serverCmd)
+}
---
+++ grpc/gateway/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gunsluo/go-example/grpc/gateway/cmd"
+
+func main() {
+	cmd.Execute()
+}
