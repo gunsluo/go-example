0b8a7fd2b13c0aac569a45298b5846c93dcf6e4a
luoji <gunsluo@gmail.com>
2018-09-25 11:28:44 +0800

    random string
    Signed-off-by: luoji <gunsluo@gmail.com>

---
+++ random/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+var seed = rand.NewSource(time.Now().UnixNano())
+
+func RandomString(n int) string {
+	b := make([]byte, n)
+	// A seed.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, seed.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = seed.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func main() {
+	fmt.Println("-->", RandomString(8))
+}
