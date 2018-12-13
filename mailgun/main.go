59ba648ced2913e668ee701568d0ed5b94dfb507
luoji <gunsluo@gmail.com>
2018-12-13 18:07:46 +0800

    send attachment
    Signed-off-by: luoji <gunsluo@gmail.com>

--- mailgun/main.go
+++ mailgun/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/mailgun/mailgun-go"
@@ -17,20 +19,48 @@ var yourDomain string = "luoji.live" // e.g. mg.yourcompany.com
 // starts with "key-"
 var privateAPIKey string = ""
 
+var mgURL string = "https://api.mailgun.net/v3"
+
+type reader struct {
+	*bytes.Reader
+}
+
+func newReader(b []byte) *reader {
+	return &reader{Reader: bytes.NewReader(b)}
+}
+
+func (r *reader) Close() error {
+	return nil
+}
+
 func main() {
 	// Create an instance of the Mailgun Client
 	mg := mailgun.NewMailgun(yourDomain, privateAPIKey)
+	mg.SetAPIBase(mgURL)
 
-	sender := "no-reply@luoji.live"
+	//sender := "no-reply@luoji.live"
+	sender := "ji.luo@target-energysolutions.com"
+	//cc := "weilong.yi@target-energysolutions.com"
+	//bcc := "xianchao.yu@target-energysolutions.com"
+	//sender := "gunsluo@gmail.com"
 	subject := "Fancy subject!"
-	body := "Hello from Mailgun Go!"
-	recipient := "ji.luo@target-energysolutions.com"
+	body := "<p>Hello from Mailgun Go!</p><p>test test</p>"
+	recipient := "gunsluo@gmail.com"
+	//recipient := "ji.luo@target-energysolutions.com"
 
-	sendMessage(mg, sender, subject, body, recipient)
-}
-
-func sendMessage(mg mailgun.Mailgun, sender, subject, body, recipient string) {
 	message := mg.NewMessage(sender, subject, body, recipient)
+	//message.AddCC(cc)
+	//message.AddBCC(bcc)
+	message.SetHtml(body)
+
+	fn := "k3.png"
+	b, err := ioutil.ReadFile(fn)
+	if err != nil {
+		panic(err)
+	}
+
+	//message.AddAttachment(fn)
+	message.AddReaderInline(fn, newReader(b))
 	resp, id, err := mg.Send(message)
 
 	if err != nil {
