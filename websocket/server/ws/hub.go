1cb5ef62a0a42194fbf9fb4c6fbca23c1d7b3514
luoji <gunsluo@gmail.com>
2020-02-20 14:06:48 +0800

    delivery message
    Signed-off-by: luoji <gunsluo@gmail.com>

--- websocket/server/ws/hub.go
+++ websocket/server/ws/hub.go
@@ -6,9 +6,6 @@ type Hub struct {
 	// Registered clients.
 	clients map[*Client]bool
 
-	// Inbound messages from the clients.
-	broadcast chan []byte
-
 	// Register requests from the clients.
 	register chan *Client
 
@@ -19,7 +16,6 @@ type Hub struct {
 // NewHub return client hub
 func NewHub() *Hub {
 	return &Hub{
-		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
 		unregister: make(chan *Client),
 		clients:    make(map[*Client]bool),
@@ -46,15 +42,6 @@ func (h *Hub) Run() {
 			if _, ok := h.clients[client]; ok {
 				delete(h.clients, client)
 				close(client.send)
-			}
-		case message := <-h.broadcast:
-			for client := range h.clients {
-				select {
-				case client.send <- message:
-				default:
-					close(client.send)
-					delete(h.clients, client)
-				}
 			}
 		}
 	}
--- websocket/server/ws/repeater.go
+++ websocket/server/ws/repeater.go
@@ -3,16 +3,19 @@ package ws
 // Repeater is a repeater
 type Repeater struct {
 	h *Hub
+
+	// Inbound messages from the clients.
+	delivery chan []byte
 }
 
 // NewRepeater return a repeater
 func NewRepeater(h *Hub) *Repeater {
-	return &Repeater{h: h}
+	return &Repeater{h: h, delivery: make(chan []byte)}
 }
 
 // Broadcast broadcast messages to the clients.
 func (r *Repeater) Broadcast(message []byte) error {
-	r.h.broadcast <- message
+	r.delivery <- message
 	return nil
 }
 
@@ -34,4 +37,27 @@ func (r *Repeater) Hub() *Hub {
 // Run start receiving channel
 func (r *Repeater) Run() {
 	go r.h.Run()
+
+	for {
+		select {
+		case message := <-r.delivery:
+			r.broadcast(message)
+		}
+	}
+}
+
+// broadcast send the message to all clients
+func (r *Repeater) broadcast(message []byte) error {
+	// TODO: 1. multithreading 2. asynchronous sending
+	h := r.Hub()
+	for client := range h.clients {
+		select {
+		case client.send <- message:
+		default:
+			close(client.send)
+			delete(h.clients, client)
+		}
+	}
+
+	return nil
 }
