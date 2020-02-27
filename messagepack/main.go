b8043174f349df586671c87b18ee53429665d325
luoji <gunsluo@gmail.com>
2020-02-27 13:08:19 +0800

    message pack
    Signed-off-by: luoji <gunsluo@gmail.com>

---
+++ messagepack/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/vmihailenco/msgpack/v4"
+)
+
+func main() {
+	type Item struct {
+		Foo string
+	}
+
+	b, err := msgpack.Marshal(&Item{Foo: "bar"})
+	if err != nil {
+		panic(err)
+	}
+
+	var item Item
+	err = msgpack.Unmarshal(b, &item)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(item.Foo)
+	// Output: bar
+}
