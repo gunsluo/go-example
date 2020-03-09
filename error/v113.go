1939e90f42c02969c315946db3f1f66a38a09321
luoji <gunsluo@gmail.com>
2020-03-09 16:17:07 +0800

    1.13 error
    Signed-off-by: luoji <gunsluo@gmail.com>

---
+++ error/v113.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Error struct {
+	Path   string
+	Reason string
+}
+
+func (e *Error) Error() string {
+	return e.Path + ": not found" + " Reason: " + e.Reason
+}
+
+var ErrNotFound = errors.New("not found")
+
+func main() {
+	{
+		e := ErrNotFound
+		w := fmt.Errorf("Path %w", e)
+		fmt.Println("error:", w)
+		fmt.Println("unwrap error:", errors.Unwrap(w))
+
+		// is
+		fmt.Println("is:", errors.Is(w, e))
+		fmt.Println("is:", errors.Is(e, w))
+	}
+
+	{
+		e := &Error{Path: "abc", Reason: "bcd"}
+		w := fmt.Errorf("Path %w", e)
+		fmt.Println("error:", w)
+		fmt.Println("unwrap error:", errors.Unwrap(w))
+
+		// is
+		fmt.Println("is:", errors.Is(w, e))
+		fmt.Println("is:", errors.Is(e, w))
+
+		var target *Error
+		if errors.As(w, &target) {
+			fmt.Println(target.Path)
+		}
+	}
+}
