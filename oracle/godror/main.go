072a488d1757676294eb209bf071728f6cd23059
luoji <gunsluo@gmail.com>
2020-04-03 17:20:26 +0800

    connection string
    Signed-off-by: luoji <gunsluo@gmail.com>

--- oracle/godror/main.go
+++ oracle/godror/main.go
@@ -5,10 +5,20 @@ import (
 	"fmt"
 
 	_ "github.com/godror/godror"
+	"github.com/xo/dburl"
 )
 
 func main() {
-	db, err := sql.Open("godror", `ac/password@127.0.0.1:1521/ORCLPDB1`)
+	//dsn := `oracle://ac/password@127.0.0.1:1521/ORCLPDB1`
+	dsn := `oracle://ac:password@127.0.0.1:1521/ORCLPDB1`
+	u, err := dburl.Parse(dsn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(u.Driver, u.DSN)
+
+	db, err := sql.Open(u.Driver, u.DSN)
 	if err != nil {
 		fmt.Println(err)
 		return
