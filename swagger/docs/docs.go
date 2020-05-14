d58e3683faed41526296127eab2fc58785c77df8
luoji <gunsluo@gmail.com>
2020-05-15 01:17:52 +0800

    go swagger
    Signed-off-by: luoji <gunsluo@gmail.com>

---
+++ swagger/docs/docs.go
@@ -0,0 +1,24 @@
+// Package classification awesome.
+//
+// Documentation of our awesome API.
+//
+//     Schemes: http
+//     BasePath: /
+//     Version: 1.0.0
+//     Host: 127.0.0.1:1323
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//
+// swagger:meta
+package docs
