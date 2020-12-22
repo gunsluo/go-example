ca818d63d022b6f05cfdf766784334d22da51488
luoji <gunsluo@gmail.com>
2020-12-22 23:41:29 +0800

    pub sub
    Signed-off-by: luoji <gunsluo@gmail.com>

--- micro/s1.go
+++ micro/s1.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/codec/bytes"
 	"github.com/micro/go-micro/v2/config"
 	"github.com/micro/go-micro/v2/config/encoder/yaml"
 	"github.com/micro/go-micro/v2/config/source"
@@ -54,9 +58,39 @@ func main() {
 
 	// initialise flags
 	service.Init()
+	// service.Server()
 
-	service.Server()
+	// create publisher
+	// p := micro.NewEvent("topic.s2", service.Client())
+	pub2s2 := micro.NewPublisher("topic.s2", service.Client())
+
+	go func() {
+		time.Sleep(5 * time.Second)
+
+		e := &Event{Message: "event"}
+		buffer, err := json.Marshal(e)
+		if err != nil {
+			panic(err)
+		}
+
+		if err := pub2s2.Publish(context.Background(), &bytes.Frame{Data: buffer}); err != nil {
+			fmt.Println("failed to publish event:", err)
+			return
+		}
+		fmt.Println("publish event")
+	}()
 
 	// start the service
-	service.Run()
+	if err := service.Run(); err != nil {
+		panic(err)
+	}
+}
+
+type Event struct {
+	// unique id
+	Id string `json:"id,omitempty"`
+	// unix timestamp
+	Timestamp int64 `json:"timestamp,omitempty"`
+	// message
+	Message string `json:"message,omitempty"`
 }
--- micro/s2.go
+++ micro/s2.go
@@ -1,26 +1,81 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/codec/bytes"
 	"github.com/micro/go-micro/v2/config"
+	"github.com/micro/go-micro/v2/config/encoder/yaml"
+	"github.com/micro/go-micro/v2/config/source"
+	"github.com/micro/go-micro/v2/config/source/file"
+	"github.com/micro/go-micro/v2/metadata"
+	"github.com/micro/go-micro/v2/util/log"
 )
 
 func main() {
+	var cfg struct {
+		Address string `json:"address"`
+	}
+
+	enc := yaml.NewEncoder()
+	config.Load(file.NewSource(
+		file.WithPath("config/s2.yaml"),
+		source.WithEncoder(enc),
+	))
+
+	// fmt.Printf("---->%#v", config.Map())
+	// var address string
+	if err := config.Get().Scan(&cfg); err != nil {
+		panic(err)
+	}
+	fmt.Println(cfg.Address)
+
+	w, err := config.Watch()
+	if err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			v, err := w.Next()
+			if err != nil {
+				panic(err)
+			}
+
+			if err := v.Scan(&cfg); err != nil {
+				panic(err)
+			}
+			fmt.Println("watch config", cfg.Address)
+		}
+	}()
+
 	// create a new service
 	service := micro.NewService(
-		micro.Name("helloworld"),
-		micro.Address(":10000"),
+		micro.Name("s2"),
+		micro.Address(cfg.Address),
 	)
 
 	// initialise flags
 	service.Init()
+	// service.Server()
 
-	service.Server()
+	// register subscriber
+	if err := micro.RegisterSubscriber("topic.s2", service.Server(), new(Sub)); err != nil {
+		panic(err)
+	}
 
 	// start the service
-	service.Run()
+	if err := service.Run(); err != nil {
+		panic(err)
+	}
+}
 
-	w, err := config.Watch("hosts", "database")
+type Sub struct{}
 
-	v, err := w.Next()
+func (s *Sub) Process(ctx context.Context, frame *bytes.Frame) error {
+	md, _ := metadata.FromContext(ctx)
+	log.Logf("[pubsub.1] Received frame %+v with metadata %+v\n", frame, md)
+	// do something with event
+	return nil
 }
