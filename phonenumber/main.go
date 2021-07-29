66134a490bb8f4aadbababcf9ad853a00c69bd3a
luoji <gunsluo@gmail.com>
2021-07-29 14:22:34 +0800

    example for phone number
    Signed-off-by: luoji <gunsluo@gmail.com>

--- phonenumber/main.go
+++ phonenumber/main.go
@@ -7,19 +7,30 @@ import (
 )
 
 func main() {
-	number := "+8618980501737"
+	number := "+96819999999999"
+	//number := "+86 18980501737"
 	// 6502530000
 	// parse our phone number
+
+	//num, err := phonenumbers.ParseAndKeepRawInput(number, "")
 	num, err := phonenumbers.Parse(number, "")
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("--------->", num)
 
-	// format it using national format
-	formattedNum := phonenumbers.Format(num, phonenumbers.NATIONAL)
+	ok := phonenumbers.IsAlphaNumber(number)
+	fmt.Println("---->", ok)
 
-	fmt.Println("---->", num.GetCountryCode(), formattedNum)
+	ok = phonenumbers.IsValidNumber(num)
+	fmt.Println("---->", ok)
+	/*
+		// format it using national format
+		formattedNum := phonenumbers.Format(num, phonenumbers.NATIONAL)
 
-	b := phonenumbers.GetRegionCodeForCountryCode(int(num.GetCountryCode()))
-	fmt.Println("---->", b)
+		fmt.Println("---->", num.GetCountryCode(), formattedNum)
+
+		b := phonenumbers.GetRegionCodeForCountryCode(int(num.GetCountryCode()))
+		fmt.Println("---->", b)
+	*/
 }
