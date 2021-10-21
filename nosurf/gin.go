d2546edf9ef0fd5bdad9a7fbd1b1ef6a120450da
luoji <gunsluo@gmail.com>
2021-10-21 11:04:18 +0800

    nosurf for gin
    Signed-off-by: luoji <gunsluo@gmail.com>

---
+++ nosurf/gin.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+
+	"github.com/gin-gonic/gin"
+	adapter "github.com/gwatts/gin-adapter"
+	"github.com/justinas/nosurf"
+)
+
+var templateString string = `
+<!doctype html>
+<html>
+<body>
+{{ if .name }}
+<p>Your name: {{ .name }}</p>
+{{ end }}
+<form action="/" method="POST">
+<input type="text" name="name">
+
+<!-- Try removing this or changing its value
+     and see what happens -->
+<input type="hidden" name="csrf_token" value="{{ .token }}">
+<input type="submit" value="Send">
+</form>
+</body>
+</html>
+`
+var templ = template.Must(template.New("t1").Parse(templateString))
+
+func index(c *gin.Context) {
+	context := make(map[string]string)
+	context["token"] = nosurf.Token(c.Request)
+
+	templ.Execute(c.Writer, context)
+}
+
+func postIndex(c *gin.Context) {
+	context := make(map[string]string)
+	context["token"] = nosurf.Token(c.Request)
+	context["name"] = c.PostForm("name")
+
+	templ.Execute(c.Writer, context)
+}
+
+func main() {
+	engine := gin.Default()
+
+	engine.Use(adapter.Wrap(nosurf.NewPure))
+	engine.GET("/", index)
+	engine.POST("/", postIndex)
+
+	fmt.Println("Listening on http://127.0.0.1:8000/")
+	engine.Run(":8000")
+}
